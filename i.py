ebca051f04df3116fa9fd00584555ee960ec956b
ruhuzursuz <48116342+ruhuzursuz@users.noreply.github.com>
2020-01-16 12:31:09 +0300

    Update i.py
    i think it's done

--- i.py
+++ i.py
@@ -4,42 +4,37 @@ import json
 import requests
 import time
 
-# requests modülü lazım. requestssiz yapmaya çalışayım mı?
-# cmd'yi açıp "pip install requests" ile kurabilirsin
+# make a text file named usernames.txt
+# put the usernames line by line
 
-# scriptin olduğu yere hesaplar.txt dosyası oluştur
-# gözlemek istediğin hesapların linklerini alt alta yaz
-
-# bence çok gereksiz bir çaba bu. ama neyse.
-
-uyuma_suresi = 300 #kaç saniyede 1 çalışsın
+sleep_time = 300
 query_hash = "f2405b236d85e8296cf30347c9f08c2a"
 
+with open("usernames.txt", "r") as usernames_file:
+    usernames = usernames_file.read().splitlines()
 
-with open("hesaplar.txt","r") as oku:
-    hesaplar = oku.read().splitlines()
 
-
-def indir(gorseller):
-    for gorsel in gorseller:
-        gorsel_id = gorsel["node"]["id"]
-        if gorsel["node"]["is_video"]:
-            shortcode = gorsel["node"]["shortcode"]
-            video_sayfasi = "https://www.instagram.com/p/" + shortcode
-            response = s.get(video_sayfasi).text
-            gorsel_link = re.findall('og:video" content="(.*?)"',response)[0]
-            uzanti = ".mp4"
+def download(images):
+    for image in images:
+        image_id = image["node"]["id"]
+        shortcode = image["node"]["shortcode"]
+        if image["node"]["is_video"]:
+            image_page_url = "https://www.instagram.com/p/" + shortcode
+            image_page_url_response = s.get(image_page_url).text
+            image_direct_url = re.search('og:video" content="(.*?)"', image_page_url_response)[1]
+            extension = ".mp4"
         else:
-            uzanti = ".jpg"
-            gorsel_link = gorsel["node"]["display_url"]
+            extension = ".jpg"
+            image_direct_url = image["node"]["display_url"]
             
-        gorsel_yolu = username + "_photos/" + gorsel_id + uzanti
-        if not os.path.isfile(gorsel_yolu):
-            response = s.get(gorsel_link)
-            with open(gorsel_yolu,"wb") as yaz:
-                yaz.write(response.content)
-            print(gorsel["node"]["shortcode"] + uzanti + " kaydedildi")
- 
+        image_file_path = username + "_images/" + image_id + extension
+        if not os.path.isfile(image_file_path):
+            image_content = s.get(image_direct_url).content
+            with open(image_file_path, "wb") as image_file:
+                image_file.write(image_content)
+            print(shortcode + extension + " saved")
+
+
 s = requests.Session()
 response = s.get("https://instagram.com/")
 csrf_token = re.search('"csrf_token":"(.*?)"', response.text)[1]
@@ -47,42 +42,39 @@ s.headers.update({"X-CSRFToken": csrf_token})
 
 while True:
     
-    for hesap in hesaplar:
-        response = s.get(hesap)
-        script = re.search('window._sharedData = (.*?);',response.text)[1]
+    for username in usernames:
+        response = s.get("https://instagram.com/" + username)
+        script = re.search('window._sharedData = (.*?);', response.text)[1]
         data = json.loads(script)
-        json_data = data["entry_data"]["ProfilePage"][0]["graphql"]["user"]
-        gizli_mi = json_data["is_private"]
-        if not gizli_mi:
-            hesap_id = json_data["id"]
+        user_data = data["entry_data"]["ProfilePage"][0]["graphql"]["user"]
+        is_private = user_data["is_private"]
+        if not is_private:
+            user_id = user_data["id"]
             has_next_page = True
             end_cursor = None
-            username = json_data["username"]
-            print(username," hesabı artık gizli değil!","\a")
+            print(username, " account is not private now!", "\a")
             if not os.path.exists(username + "_photos"):
                 os.mkdir(username + "_photos")
 
             while has_next_page:
                 if end_cursor is None:
-                    has_next_page = json_data["edge_owner_to_timeline_media"]["page_info"]["has_next_page"]
-                    end_cursor = json_data["edge_owner_to_timeline_media"]["page_info"]["end_cursor"]
-                    gorseller = json_data["edge_owner_to_timeline_media"]["edges"]
-                    indir(gorseller)
+                    has_next_page = user_data["edge_owner_to_timeline_media"]["page_info"]["has_next_page"]
+                    end_cursor = user_data["edge_owner_to_timeline_media"]["page_info"]["end_cursor"]
+                    pics_and_vids = user_data["edge_owner_to_timeline_media"]["edges"]
+                    download(pics_and_vids)
                 
                 if end_cursor is not None:
-                    variables = '{"id":'+hesap_id+',"first":12,"after":"'+end_cursor+'"}'
-                    payload = {"query_hash": query_hash,"variables" : variables}
-                    response = s.get("https://www.instagram.com/graphql/query/?", params = payload).json()
-                    gorseller = response["data"]["user"]["edge_owner_to_timeline_media"]["edges"]
-                    indir(gorseller)
-                    has_next_page = response["data"]["user"]["edge_owner_to_timeline_media"]["page_info"]["has_next_page"]
-                    end_cursor = response["data"]["user"]["edge_owner_to_timeline_media"]["page_info"]["end_cursor"]
-            print(username, " hesabının görsellerini güncelledim\n")
-            print("Kendisiyle işimiz bitti gibi. Linkini text dosyasından silmeyi düşünebilirsin","\a")
-            print("Aksi takdirde sürekli fotoğraflarını kontrol edeceğim\n")
+                    variables = '{"id":' + user_id + ',"first":12,"after":"' + end_cursor + '"}'
+                    payload = {"query_hash": query_hash, "variables": variables}
+                    resp = s.get("https://www.instagram.com/graphql/query/?", params=payload).json()
+                    pics_and_vids = resp["data"]["user"]["edge_owner_to_timeline_media"]["edges"]
+                    download(pics_and_vids)
+                    has_next_page = resp["data"]["user"]["edge_owner_to_timeline_media"]["page_info"]["has_next_page"]
+                    end_cursor = resp["data"]["user"]["edge_owner_to_timeline_media"]["page_info"]["end_cursor"]
+            print(username, " images are updated\n")
+            print("you can delete username from the file", "\a")
+            print("otherwise i will check the same username againg and again\n")
+            print("it is up to you\n")
 
-    print("Hesapları kontrol ettim. Biraz uyuyacağım")
-    time.sleep(uyuma_suresi)
-                
-                
-                    
+    print("i've checked the accounts. i'll sleep now")
+    time.sleep(sleep_time)
